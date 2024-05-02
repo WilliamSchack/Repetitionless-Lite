8bce99ebb723dd7636970535c3169cd639c60c26
William Schack <54973797+WilliamSchack@users.noreply.github.com>
2024-05-02 23:40:26 +1000

    Rotate & Scale
    COMPLETE
    - Rotate textures by random amount
    - Scale textures by random amount
    - Cover rotated seams
    
    TODO
    - Distance based blending
    - Multiple texture merging (Detail texture)
    - Test on terrain
    - Test on URP and HDRP
    - Add custom inspector

---
+++ True Seamless Texturing/Assets/CustomVoronoi.hlsl
@@ -0,0 +1,60 @@
+inline float2 randomVector (float2 UV, float offset)
+{
+    float2x2 m = float2x2(15.27, 47.63, 99.41, 89.98);
+    UV = frac(sin(mul(UV, m)) * 46839.32);
+    return float2(sin(UV.y*+offset)*0.5+0.5, cos(UV.x*offset)*0.5+0.5);
+}
+
+float rand2dTo1d(float2 value, float2 dotDir = float2(12.9898, 78.233))
+{
+    float2 smallValue = sin(value);
+    float random = dot(smallValue, dotDir);
+    random = frac(sin(random) * 143758.5453);
+    return random;
+}
+
+// Based on code by Inigo Quilez: https://iquilezles.org/articles/voronoilines/
+void CustomVoronoi_float(float2 UV, float AngleOffset, float CellDensity, out float DistFromCenter, out float DistFromEdge, out float Cells)
+{
+    int2 cell = floor(UV * CellDensity);
+    float2 posInCell = frac(UV * CellDensity);
+    
+    DistFromCenter = 8.0f;
+    float2 closestOffset;
+    float2 closestCell;
+    
+    for(int y = -1; y <= 1; ++y)
+    {
+        for (int x = -1; x <= 1; ++x)
+        {
+            int2 cellToCheck = int2(x, y);
+            float2 cellOffset = float2(cellToCheck) - posInCell + randomVector(cell + cellToCheck, AngleOffset);
+            
+            float distToPoint = dot(cellOffset, cellOffset);
+
+            if (distToPoint < DistFromCenter)
+            {
+                DistFromCenter = distToPoint;
+                closestOffset = cellOffset;
+                closestCell = cellToCheck;
+            }
+        }
+    }
+    
+    Cells = randomVector(cell + closestCell, AngleOffset);
+    
+    DistFromEdge = 8.0f;
+
+    for(int y = -1; y <= 1; ++y)
+    {
+        for(int x = -1; x <= 1; ++x)
+        {
+            int2 cellToCheck = int2(x, y);
+            float2 cellOffset = float2(cellToCheck) - posInCell + randomVector(cell + cellToCheck, AngleOffset);
+            
+            float distToEdge = dot(0.5f * (closestOffset + cellOffset), normalize(cellOffset - closestOffset));
+
+            DistFromEdge = min(DistFromEdge, distToEdge);
+        }
+    }
+}
