866a51f2404b32fa9adb515f9ae16b81f870a969
William Schack <wil.schack@gmail.com>
2025-12-11 18:23:04 +1100

    Added rudimentary layer selection and viewing in terrain editor

--- True Seamless Texturing/Packages/com.williamschack.repetitionless/Editor/Inspectors/RepetitionlessMaterialEditorTerrainNEW.cs
+++ True Seamless Texturing/Packages/com.williamschack.repetitionless/Editor/Inspectors/RepetitionlessMaterialEditorTerrainNEW.cs
@@ -1,37 +1,81 @@
 #if UNITY_EDITOR
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
 namespace Repetitionless.Inspectors
 {
+    using GUIUtilities;
+    using Data;
+
     public class RepetitionlessMaterialEditorTerrainNEW : RepetitionlessMaterialEditorBaseNEW
     {
         protected override int _maxLayers => 32;
+
+        private List<TerrainLayer> _terrainLayers;
+
+        private int _currentLayerIndex = 0;
+        private bool _showingTerrainLayers = false;
+
+        public override void OnEnable(MaterialEditor materialEditor)
+        {
+            base.OnEnable(materialEditor);
+
+            // Load terrain layers
+            TerrainLayerSyncDataSO syncData = TerrainLayerSyncDataSO.Load();
+            _terrainLayers = syncData.MaterialToTerrainLayer.Get(_material).Items;
+        }
 
         public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
         {
             base.OnGUI(materialEditor, properties);
 
-            // TESTING
-            for (int i = 0; i < 2; i++) {
-                // Base Material
-                DrawBaseMaterialGUI(i);
+            DrawTerrainSettings();
 
-                GUILayout.Space(SETTING_PADDING);
+            GUILayout.Space(SETTING_PADDING);
 
-                // Distance Blend Material
-                DrawDistanceBlendGUI(i);
+            // Base Material
+            DrawBaseMaterialGUI(_currentLayerIndex);
 
-                GUILayout.Space(SETTING_PADDING);
+            GUILayout.Space(SETTING_PADDING);
 
-                // Material Blend
-                DrawMaterialBlendGUI(i);
+            // Distance Blend Material
+            DrawDistanceBlendGUI(_currentLayerIndex);
 
-                GUILayout.Space(SETTING_PADDING);
-            }
+            GUILayout.Space(SETTING_PADDING);
+
+            // Material Blend
+            DrawMaterialBlendGUI(_currentLayerIndex);
+
+            GUILayout.Space(SETTING_PADDING);
 
             // Footer Settings
             DrawDebugGUI();
+        }
+
+        private void DrawTerrainSettings()
+        {
+            GUIUtilities.BeginBackgroundVertical();
+
+            GUIUtilities.DrawHeaderLabelLarge("Terrain");
+
+            _currentLayerIndex = EditorGUILayout.IntSlider("Editing Layer", _currentLayerIndex, 0, _terrainLayers.Count - 1);
+
+            GUILayout.Space(5);
+
+            GUIUtilities.BeginBackgroundVertical();
+            _showingTerrainLayers = GUIUtilities.DrawFoldout(_showingTerrainLayers, "Terrain Layers");
+            if (_showingTerrainLayers) {
+                GUILayout.Label("These can be changed in the terrain");
+
+                GUI.enabled = false;
+                for (int i = 0; i < _terrainLayers.Count; i++) {
+                    EditorGUILayout.ObjectField($"Terrain Layer {i}", _terrainLayers[i], typeof(TerrainLayer), false);
+                }
+                GUI.enabled = true;
+            }
+            GUIUtilities.EndBackgroundVertical();
+            GUIUtilities.EndBackgroundVertical();
         }
     }
 }
