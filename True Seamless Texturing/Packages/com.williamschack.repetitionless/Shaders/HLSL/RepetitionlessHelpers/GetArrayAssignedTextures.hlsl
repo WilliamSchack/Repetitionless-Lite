e190d82c68d6846f91c5fe172977e5b82464b9d9
William Schack <wil.schack@gmail.com>
2025-12-16 15:08:16 +1100

    Fixed assigned textures not working properly
    Now accounting for floating point precision errors

--- True Seamless Texturing/Packages/com.williamschack.repetitionless/Shaders/HLSL/RepetitionlessHelpers/GetArrayAssignedTextures.hlsl
+++ True Seamless Texturing/Packages/com.williamschack.repetitionless/Shaders/HLSL/RepetitionlessHelpers/GetArrayAssignedTextures.hlsl
@@ -12,13 +12,15 @@ void GetArrayAssignedTextures(UnityTexture2D tex,
     // Assigned array textures stored in two pixels, 16bits each from 0-1
     // Reconstruct into 32bit integers
 
-    int3 assignedAV0  = tex.Load(int3(0, 0, 0)).rgb * 65535;
-    int3 assignedAV1  = tex.Load(int3(1, 0, 0)).rgb * 65535;
-    int3 assignedNSO0 = tex.Load(int3(0, 1, 0)).rgb * 65535;
-    int3 assignedNSO1 = tex.Load(int3(1, 1, 0)).rgb * 65535;
-    int3 assignedEM0  = tex.Load(int3(0, 2, 0)).rgb * 65535;
-    int3 assignedEM1  = tex.Load(int3(1, 2, 0)).rgb * 65535;
-    int3 assignedBM   = tex.Load(int3(0, 3, 0)).rgb * 65535;
+    // * 65535 for 0-1 colour to 0-16bit int
+    // + 0.5 for float precision
+    int3 assignedAV0  = (int3)(tex.Load(int3(0, 0, 0)).rgb * 65535 + 0.5);
+    int3 assignedAV1  = (int3)(tex.Load(int3(1, 0, 0)).rgb * 65535 + 0.5);
+    int3 assignedNSO0 = (int3)(tex.Load(int3(0, 1, 0)).rgb * 65535 + 0.5);
+    int3 assignedNSO1 = (int3)(tex.Load(int3(1, 1, 0)).rgb * 65535 + 0.5);
+    int3 assignedEM0  = (int3)(tex.Load(int3(0, 2, 0)).rgb * 65535 + 0.5);
+    int3 assignedEM1  = (int3)(tex.Load(int3(1, 2, 0)).rgb * 65535 + 0.5);
+    int3 assignedBM   = (int3)(tex.Load(int3(0, 3, 0)).rgb * 65535 + 0.5);
 
     AssignedAVTextures[0] = Combine16BitInts(assignedAV0.r, assignedAV1.r);
     AssignedAVTextures[1] = Combine16BitInts(assignedAV0.g, assignedAV1.g);
