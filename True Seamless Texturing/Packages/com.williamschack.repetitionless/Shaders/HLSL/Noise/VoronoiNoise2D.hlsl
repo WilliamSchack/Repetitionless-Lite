2ac598a6ab07cdd936290aed381c98bc0c0a11b8
William Schack <wil.schack@gmail.com>
2026-01-07 16:00:43 +1100

    Optimized voronoi noise

--- True Seamless Texturing/Packages/com.williamschack.repetitionless/Shaders/HLSL/Noise/VoronoiNoise2D.hlsl
+++ True Seamless Texturing/Packages/com.williamschack.repetitionless/Shaders/HLSL/Noise/VoronoiNoise2D.hlsl
@@ -1,34 +1,36 @@
 #ifndef REPETITIONLESSVORONOINOISE_INCLUDED
 #define REPETITIONLESSVORONOINOISE_INCLUDED
 
-inline float2 randomVector (float2 UV, float offset)
+// hash22 under MIT License Copyright (c)2014 David Hoskins
+// https://www.shadertoy.com/view/4djSRW
+inline float2 hash22(float2 p2)
 {
-    float2x2 m = float2x2(15.27, 47.63, 99.41, 89.98);
-    UV = frac(sin(mul(UV, m)) * 46839.32);
-    return float2(sin(UV.y*+offset)*0.5+0.5, cos(UV.x*offset)*0.5+0.5);
+    float3 p3 = frac(float3(p2.xyx) * float3(.1031, .1030, .0973));
+    p3 += dot(p3, p3.yzx + 33.33);
+    return frac((p3.xx + p3.yz) * p3.zy);
 }
 
 // Based on code by Inigo Quilez: https://iquilezles.org/articles/voronoilines/
 void VoronoiNoise(float2 UV, float AngleOffset, float CellDensity, out float DistFromCenter, out float DistFromEdge, out float Cells)
 {
-    int2 cell = floor(UV * CellDensity);
-    float2 posInCell = frac(UV * CellDensity);
+    float2 uvScaled = UV * CellDensity;
+    int2 cell = floor(uvScaled);
+    float2 posInCell = frac(uvScaled);
     
     DistFromCenter = 8.0f;
     float2 closestOffset;
     float2 closestCell;
-    
-    for(int y1 = -1; y1 <= 1; ++y1)
-    {
-        for (int x1 = -1; x1 <= 1; ++x1)
-        {
-            int2 cellToCheck = int2(x1, y1);
-            float2 cellOffset = float2(cellToCheck) - posInCell + randomVector(cell + cellToCheck, AngleOffset);
-            
+
+    [unroll]
+    for(int y1 = -1; y1 <= 1; y1++) {
+        [unroll]
+        for (int x1 = -1; x1 <= 1; x1++) {
+            float2 cellToCheck = float2(x1, y1);
+            float2 cellOffset = cellToCheck - posInCell + hash22(cell + cellToCheck + AngleOffset);
+
             float distToPoint = dot(cellOffset, cellOffset);
 
-            if (distToPoint < DistFromCenter)
-            {
+            if (distToPoint < DistFromCenter) {
                 DistFromCenter = distToPoint;
                 closestOffset = cellOffset;
                 closestCell = cellToCheck;
@@ -36,17 +38,17 @@ void VoronoiNoise(float2 UV, float AngleOffset, float CellDensity, out float Dis
         }
     }
     
-    Cells = randomVector(cell + closestCell, AngleOffset).x;
-    
+    Cells = hash22(cell + closestCell + AngleOffset).x;
     DistFromEdge = 8.0f;
 
-    for(int y2 = -1; y2 <= 1; ++y2)
-    {
-        for(int x2 = -1; x2 <= 1; ++x2)
-        {
-            int2 cellToCheck = int2(x2, y2);
-            float2 cellOffset = float2(cellToCheck) - posInCell + randomVector(cell + cellToCheck, AngleOffset);
+    [unroll]
+    for(int y2 = -1; y2 <= 1; y2++) {
+        [unroll]
+        for(int x2 = -1; x2 <= 1; x2++) {
+            float2 cellToCheck = float2(x2, y2);
+            float2 cellOffset = cellToCheck - posInCell + hash22(cell + cellToCheck + AngleOffset);
             
+            float2 diff = cellOffset - closestOffset;
             float distToEdge = dot(0.5f * (closestOffset + cellOffset), normalize(cellOffset - closestOffset));
 
             DistFromEdge = min(DistFromEdge, distToEdge);
