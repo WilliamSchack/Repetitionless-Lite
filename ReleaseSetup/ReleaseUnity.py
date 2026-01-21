010d9e809bbd7e774649cec2bb37343978f1d536
William Schack <wil.schack@gmail.com>
2026-01-21 20:19:24 +1100

    Updated release script
    Not submitting, I will test that when I need an update

--- ReleaseSetup/ReleaseUnity.py
+++ ReleaseSetup/ReleaseUnity.py
@@ -5,11 +5,31 @@ import UnityPublisherAPI as UnityAPI
 unityEmail = sys.argv[1]
 unityPass = sys.argv[2]
 
+newVersionTag = sys.argv[3]
+releaseNotesHtmlFile = sys.argv[4]
+
+# Read html file
+releaseNotesHtml = open(releaseNotesHtmlFile, "r").read()
+releaseNotesHtml = releaseNotesHtml.replace("\n", "")
+
+# Update package details
 print("Authorising...")
 session = UnityAPI.Authorisation.GetAuthenticatedSession(unityEmail, unityPass)
-print("Successfully Authorised")
+print("Successfully Authorised!")
 
+print("Creating/Getting Draft...")
 packageId = UnityAPI.Packages.GetPackageId(session, "Repetitionless")
 draftId = UnityAPI.Packages.GetDraftId(session, packageId)
 
-print(draftId)
+print("Updating Draft...")
+UnityAPI.Packages.UpdateDraft(session, draftId, {
+    "version_name": newVersionTag,
+    "metadata": [
+        {
+            "release_notes": releaseNotesHtml
+        }
+    ]
+})
+
+# print("Submitting")
+# UnityAPI.Packages.SubmitDraft(session, draftId, False, "")
