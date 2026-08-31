e4cfc75e7bccb55d76c102735b61a53deffc1e22
William Schack <54973797+WilliamSchack@users.noreply.github.com>
2026-08-31 16:44:00 +1000

    Added samplegrad overload for SampleRepetitionlessTexture

--- True Seamless Texturing/Packages/com.williamschack.repetitionless/Shaders/Common/RepetitionlessHelpers/RepetitionlessTextureUtilities.hlsl
+++ True Seamless Texturing/Packages/com.williamschack.repetitionless/Shaders/Common/RepetitionlessHelpers/RepetitionlessTextureUtilities.hlsl
@@ -6,6 +6,8 @@
 
 // Samples the base and edge colour if required and lerps them together
 // Uses a regular texture
+
+// Regular overload
 float4 SampleRepetitionlessTexture(
     Texture2D Texture,
     SamplerState SS,
@@ -28,6 +30,38 @@ float4 SampleRepetitionlessTexture(
 
     if (SampleEdge) {
         float4 edgeTextureColor = SAMPLE_TEXTURE2D(Texture, SS, EdgeUV);
+        if (NormalMap) edgeTextureColor.rgb = UnpackNormalMap(edgeTextureColor, NormalStrength);
+        baseTextureColor = lerp(baseTextureColor, edgeTextureColor, EdgeMask);
+    }
+
+    return baseTextureColor;
+}
+
+// SampleGrad overload
+float4 SampleRepetitionlessTexture(
+    Texture2D Texture,
+    SamplerState SS,
+    float2 DdxUV, float2 DdyUV,
+    float2 EdgeDdxUV, float2 EdgeDdyUV,
+    
+    float EdgeMask,
+    float2 EdgeUV,
+    float2 TransformedUV,
+    bool SampleEdge,
+
+    bool NormalMap = false,
+    float NormalStrength = 1.0
+){
+    float4 baseTextureColor = 1;
+
+    // Only sample base material if visible
+    if (!SampleEdge || (SampleEdge && EdgeMask != 1))
+        baseTextureColor = Texture.SampleGrad(SS, TransformedUV, DdxUV, DdyUV);
+
+    if (NormalMap) baseTextureColor.rgb = UnpackNormalMap(baseTextureColor, NormalStrength);
+
+    if (SampleEdge) {
+        float4 edgeTextureColor = Texture.SampleGrad(SS, EdgeUV, EdgeDdxUV, EdgeDdyUV);
         if (NormalMap) edgeTextureColor.rgb = UnpackNormalMap(edgeTextureColor, NormalStrength);
         baseTextureColor = lerp(baseTextureColor, edgeTextureColor, EdgeMask);
     }
